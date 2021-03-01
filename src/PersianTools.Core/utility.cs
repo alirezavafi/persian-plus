8942e9e37942d59c5d07efc659ca0f877fa077e7
Alireza Vafi <ar.vafi@gmail.com>
2021-03-01 09:03:00 +0330

    temp

---
+++ src/PersianTools.Core/utility.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace PersianTools.Core
+{
+	public static class Utility
+	{
+		/// <summary>
+		/// Get Enum Description
+		/// </summary>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		public static string GetDescription(this Enum value)
+		{
+			Type type = value.GetType();
+			string name = Enum.GetName(type, value);
+			if (name != null)
+			{
+				FieldInfo field = type.GetField(name);
+				if (field != null)
+				{
+					DescriptionAttribute attr =
+						   Attribute.GetCustomAttribute(field,
+							 typeof(DescriptionAttribute)) as DescriptionAttribute;
+					if (attr != null)
+					{
+						return attr.Description;
+					}
+				}
+			}
+			return null;
+		}
+	}
+}
